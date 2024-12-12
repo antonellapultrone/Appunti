50b0f38849d0e3c13012a6f3a3e1192ff0800889
Dante Lugo <dantelugo1505@gmail.com>
2024-12-12 20:16:10 -0300

    cambio de nombre de variables de entorno

--- src/config/conection.js
+++ src/config/conection.js
@@ -4,10 +4,11 @@ import dotenv from 'dotenv';
 dotenv.config({ path: './credenciales.env' });
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.MYSQL_ADDON_HOST,
+    user: process.env.MYSQL_ADDON_USER,
+    password: process.env.MYSQL_ADDON_PASSWORD,
+    database: process.env.MYSQL_ADDON_DB,
+    port: process.env.MYSQL_ADDON_PORT,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
